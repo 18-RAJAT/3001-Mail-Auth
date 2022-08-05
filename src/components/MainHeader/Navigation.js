da3696f10e02067baf6e6104cc0dca5193a6a917
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-05 18:07:09 +0530

    adding useEffect

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -2,29 +2,28 @@ import React from 'react';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) =>{
-    return (
-        <nav className={classes.nav}>
-        <ul>
-        {props.isLoggedIn &&(
-            <li>
-            <a href="/"></a>
-            </li>
+const Navigation = (props) => {
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
         )}
+        {props.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {props.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
-    {props.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {props.isLoggedIn && (
-              <li>
-                <button onClick={props.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
-      );
-    };
-
-    export default Navigation;
+export default Navigation;
