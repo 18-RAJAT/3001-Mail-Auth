951830396699f08e8f0f357f3392973e90c52e24
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-05 16:49:03 +0530

    first commit

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from '../UI/Card/Card';
+import classes from './Hom.module/css';
+
+const Home=(props)=>{
+    return(
+    <Card className={classes.home}>
+    <h1>Welcome...!!</h1>
+    </Card>
+    );
+};
