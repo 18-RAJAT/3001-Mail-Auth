e3674f7b893ce9f820aad8e4cfc37efed81311d8
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-10 11:28:08 +0530

    auth handle the states

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const AuthContext=React.createContext({
     isLoggedIn:false,
     onLogout:()=>{}
 });
+
+export const AuthContextProvider=({props})=>{
+    const[isLoggedIn,setIsLoggedIn]=useState(false);
+    
+    const logoutHandler=()=>{
+        setIsLoggedIn(false);
+    };
+    const loginHandler=()=>{
+        setIsLoggedIn(true);
+    };
+    
+    return <AuthContext.Provider value={{isLoggedIn:isLoggedIn,onLogout:logoutHandler,onLogin:loginHandler}}>{props.children}</AuthContext.Provider>
+};
 
 export default AuthContext;
