25341f8b2d874fdf777fcd7607dbb8e324089bb5
18-RAJAT <Rajatjoshi7535@gmail.com>
2022-08-10 12:47:07 +0530

    Navigation js fix

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,3 +1,40 @@
+//using useContext
+
+import React, { useContext } from 'react';
+
+import AuthContext from '../../store/auth-context';
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
+
+  return (
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={ctx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
+
+
+
 // import React from 'react'
 // import AuthContext from '../../store/auth-context';
 // import classes from './Navigation.module.css';
@@ -33,41 +70,3 @@
 // };
 
 // export default Navigation;
-
-
-
-
-//using useContext
-
-import React, { useContext } from 'react';
-
-import AuthContext from '../../store/auth-context';
-import classes from './Navigation.module.css';
-
-const Navigation = (props) => {
-  const ctx = useContext(AuthContext);
-
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        {ctx.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {ctx.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {ctx.isLoggedIn && (
-          <li>
-            <button onClick={ctx.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
-
-export default Navigation;
